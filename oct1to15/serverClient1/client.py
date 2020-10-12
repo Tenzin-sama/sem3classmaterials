081bf8adbe02f3121b74b5dc8001177db31efccf
myusername <tenzin.zeta@gmail.com>
2020-10-12 11:43:22 +0545

    Some optimizations

--- oct1to15/serverClient1/client.py
+++ oct1to15/serverClient1/client.py
@@ -6,13 +6,13 @@ from threading import *
 class ServerSide:
 
     def __init__(self):
-        global conn, runstate
+        global conn, runstate  # shared variable across all modules
 
-        runstate = True
+        runstate = True  # variable to dictate if loop is to keep running or stop
 
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # (internet ver 4, socket TCP/IP)
         conn.connect(('192.168.1.2', 8081))  # ((IP address, post))
-        print("Connection Established\n")
+        print("Connection Established\n")  # if connected to server
 
         server_message = "<A Client has connected>\n"
         data = server_message.encode()
@@ -25,9 +25,12 @@ class ServerSide:
         threadSend.start()
 
         threadReceive.join()
+        input("Log: Communications have stopped. Press enter twice to end the program\n")
         threadSend.join()
 
+
     def sendData(self):
+        """to send data to server"""
         global conn, runstate
         while runstate:
             client_message = "Client: " + input()
@@ -38,6 +41,7 @@ class ServerSide:
             conn.send(client_data)
 
     def receiveData(self):
+        """to receive data from server"""
         global conn, runstate
         while runstate:
             server_data = conn.recv(1024)
--- oct1to15/serverClient1/server.py
+++ oct1to15/serverClient1/server.py
@@ -6,32 +6,37 @@ from threading import *
 class ServerSide:
 
     def __init__(self):
-        global conn, runstate
+        global conn, runstate  # shared variable across all modules
+
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind(('192.168.1.2', 8081))  # ((IP address, post))
         server_socket.listen()  # continuously listens to requests on binded (address,port)
         print("Waiting for connection...")
         conn, address = server_socket.accept()
-        print("Connection Established with", address, "\n")
+        # print("Connection Established with", address, "\n")  # after a client connects
 
-        runstate = True
+        runstate = True  # to dictate whether to continue running or stop
 
         server_message = "<You have connected with Server>\n"
         data = server_message.encode()
         conn.send(data)
 
+        # using threads to run both receive and send in parallel
         threadReceive = Thread(target=self.receiveData)
         threadSend = Thread(target=self.sendData)
 
         threadReceive.start()
         threadSend.start()
 
+        # when the threaded functions have stopped
         threadReceive.join()
-        print("Receive has stopped")
+        input("Log: Receive has stopped. Press enter twice to end the program\n")
         threadSend.join()
-        print("Send has stopped")
+        print("Log: Send has stopped. Program has ended")
+        server_socket.close()
 
     def sendData(self):
+        """funtion to send data to client"""
         global conn, runstate
         while runstate:
             server_message = "Server: " + input()
@@ -42,11 +47,12 @@ class ServerSide:
             conn.send(server_data)
 
     def receiveData(self):
+        """function to receive data from client"""
         global conn, runstate
         while runstate:
             client_data = conn.recv(1024)
             client_message = client_data.decode()
-            if client_message == "Client: x":
+            if client_message == "Client: x":  # if client has disconnected
                 print("[Client has disconnected]")
                 runstate = False
             if runstate:
